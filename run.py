8af6e8ba23bb738c17ed9c685365c9eade47e31c
xingyu Fu <30400802+fxy96@users.noreply.github.com>
2018-06-06 09:00:52 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,13 @@
 """
-Stock Selection ----- Deep or Traditional Methods?
+A Machine Learning Framework for Stock Selection
     
 Authors:
 XingYu Fu; JinHong Du; YiFeng Guo; MingWen Liu; Tao Dong; XiuWen Duan; 
-ZiYi Yang;
 
 Institutions:
-AI&Fintech Lab of Gradient Trading; 
+AI&Fintech Lab of Likelihood Technology; 
+Gradient Trading;
 School of Mathematics, Sun Yat-sen University;
-LingNan College, Sun Yat-sen University;
 
 Contact:
 fuxy28@mail2.sysu.edu.cn

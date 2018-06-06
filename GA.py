35b1a60c70efb71596ac0cffa47b365cb1c00d5c
xingyu Fu <30400802+fxy96@users.noreply.github.com>
2018-06-06 09:05:16 +0800

    Update GA.py

--- GA.py
+++ GA.py
@@ -5,9 +5,9 @@ Authors:
 XingYu Fu; JinHong Du; YiFeng Guo; MingWen Liu; Tao Dong; XiuWen Duan; 
 
 Institutions:
-AI&Fintech Lab of Gradient Trading; 
+AI&Fintech Lab of Likelihood Technology; 
+Gradient Trading;
 School of Mathematics, Sun Yat-sen University;
-LingNan College, Sun Yat-sen University;
 
 Contact:
 fuxy28@mail2.sysu.edu.cn
